0c1c30b42d2113cdfbac3b4c0a51279ca2499eb7
TRAVIS GRIZZEL <grizzel@TRAVISs-iMac.local>
2014-01-10 23:22:15 -0800

    Submit to class

--- assignments/session01/echo_client.py
+++ assignments/session01/echo_client.py
@@ -6,16 +6,19 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate 
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
     print >>log_buffer, 'connecting to {0} port {1}'.format(*server_address)
     # TODO: connect your socket to the server here.
-
+    sock.connect(server_address)
     # this try/finally block exists purely to allow us to close the socket
     # when we are finished with it
     try:
         print >>log_buffer, 'sending "{0}"'.format(msg)
         # TODO: send your message to the server here.
-
+        sock.sendall(msg)
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks.  You will want to log them as you receive 
         #       each one.  You will also need to check to make sure that 
@@ -24,17 +27,22 @@ def client(msg, log_buffer=sys.stderr):
         # 
         #       Make sure that you log each chunk you receive.  Use the print 
         #       statement below to do it. (The tests expect this log format)
-        chunk = ''
-        print >>log_buffer, 'received "{0}"'.format(chunk)
+        full_msg = ''
+        done = False
+        chunk = sock.recv(16)
+        if len(chunk) < 16:
+            print >>log_buffer, 'received "{0}"'.format(chunk)
+            done = True
+        full_msg += msg
     finally:
         # TODO: after you break out of the loop receiving echoed chunks from 
         #       the server you will want to close your client socket.
         print >>log_buffer, 'closing socket'
-
+        sock.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        usg = '\nusage: python echo_client.py "this is my message"\n'
+        usg = '\nusage: python echo_client.py "This is my message.  What do you think about that?"\n'
         print >>sys.stderr, usg
         sys.exit(1)
     
